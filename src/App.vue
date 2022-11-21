a97c258946425f318b504e9de6cc4008ab4547b8
popetrov <popetrov64@gmail.com>
2022-11-21 18:03:12 +0400

    added pages

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,10 @@
         <h1>
             Страница с постами
         </h1>
+        <MyInput
+            v-model="searchQuery"
+            placeholder="Поиск..."
+        />
         <div class="app_btns">
             <MyButton 
                 class="btn_create" 
@@ -21,11 +25,24 @@
             />
         </MyDialog>
         <PostListVue 
-            :posts="sortedPosts"
+            :posts="sortedAndSearchedPosts"
             @remove="removePost"
             v-if="!isPostLoading"
         />
         <div v-else>Идет загрузка...</div>
+        <div class="page-wrapper">
+            <div 
+                v-for="pageNumber in totalPage"
+                :key="pageNumber"
+                class="page"
+                :class="{
+                    'current-page': page === pageNumber
+                }"
+                @click="changePage(pageNumber)"
+            >
+                {{pageNumber}}
+            </div>
+        </div>
     </div>
 </template>
 
@@ -52,6 +69,10 @@ export default {
             dialogVisible: false,
             isPostLoading: false,
             selectedSort: '',
+            searchQuery:'',
+            page: 1,
+            limit: 10,
+            totalPage: 0,
             sortOptions: [
                 {value: 'title', name: 'По названию'},
                 {value: 'body', name: 'По содержимому'}
@@ -72,7 +93,13 @@ export default {
         async fetchPosts() {
             try {
                 this.isPostLoading = true
-                const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10')
+                const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
+                    params: {
+                        _page: this.page,
+                        _limit: this.limit
+                    }
+                })
+                this.totalPage = Math.ceil(response.headers['x-total-count']/this.limit)
                 this.posts = response.data
             }catch(error){
                 console.log(error)
@@ -80,6 +107,10 @@ export default {
                 this.isPostLoading = false
             }
         },
+        changePage(pageNumber) {
+            this.page = pageNumber
+            this.fetchPosts()
+        }
     },
     mounted() {
             this.fetchPosts()
@@ -87,6 +118,9 @@ export default {
     computed: {
         sortedPosts() {
             return [...this.posts].sort((post1, post2) => (post1[this.selectedSort])?.localeCompare(post2[this.selectedSort]))
+        },
+        sortedAndSearchedPosts() {
+            return this.sortedPosts.filter(post => post.title.toLowerCase().includes(this.searchQuery.toLowerCase()))
         }
     },
     watch: {
@@ -112,5 +146,23 @@ export default {
         margin: 10px 0;
         display: flex;
         justify-content: space-between;
+    }
+
+    .page-wrapper {
+        display: flex;
+        margin-top: 15px;
+        justify-content: center;
+    }
+
+    .page {
+        margin-left: 5px;
+        border: 1px solid black;
+        padding: 10px;
+        cursor: pointer;
+    }
+
+    .current-page {
+        border: 2px solid teal;
+        cursor: pointer;
     }
 </style>
