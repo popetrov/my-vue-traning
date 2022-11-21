c8bc367d735371cd36fbe7763e1e7a316bb22949
popetrov <popetrov64@gmail.com>
2022-11-21 12:55:32 +0400

    added animation added and remove post and sorted posts

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,7 +1,7 @@
 <template>
     <div v-if="posts.length > 0">
         <h3>Список постов</h3>
-        <transition-group>
+        <transition-group name="post-list">
             <PostItem
                 v-for="post in posts"
                 :post="post" 
@@ -32,5 +32,22 @@ import PostItem from './PostItem.vue';
 <style scoped> 
     .no_post {
         color: red;
+    }
+
+    .post-list-item {
+        display: inline-block;
+        margin-right: 10px;
+    }
+    .post-list-enter-active,
+    .post-list-leave-active {
+        transition: all 0.6s ease;
+    }
+    .post-list-enter-from,
+    .post-list-leave-to {
+        opacity: 0;
+        transform: translateX(130px);
+    }
+    .post-list-move {
+        transition: transform 0.5s ease;
     }
 </style>
